5fb011bef786e8e133aa665a7a083808251d1616
Jenish Korat <jenishkorat3@gmail.com>
2023-11-03 21:52:00 +0530

    splash screen added

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,43 @@
+import 'dart:async';
+
+import 'package:arogya_mitra/auth/login_screen.dart';
+import 'package:arogya_mitra/screens/home_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  final auth = FirebaseAuth.instance;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    final user = auth.currentUser;
+
+    if (user != null) {
+      Timer(Duration(seconds: 3), () => Navigator.push(context, MaterialPageRoute(builder: (context) => HomeScreen())));
+    } else {
+      Timer(Duration(seconds: 3), () => Navigator.push(context, MaterialPageRoute(builder: (context) => LoginPage())));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+          child: Center(
+        child: Image.asset(
+          'assets/images/heart.png',
+        ),
+      )),
+    );
+  }
+}
