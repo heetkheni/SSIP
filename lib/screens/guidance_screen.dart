c943d302e52def9736ecf64ac21360dfb43e90bf
Purvik <purviksukhadiya99091@gmail.com>
2023-11-04 15:04:47 +0530

    guidence screen bmi updated

--- lib/screens/guidance_screen.dart
+++ lib/screens/guidance_screen.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:arogya_mitra/data/guidanceData.dart';
 import 'package:arogya_mitra/screens/guidance_category_screen.dart';
@@ -11,8 +13,24 @@ class GuidanceScreen extends StatefulWidget {
 
 class _GuidanceScreenState extends State<GuidanceScreen> {
   int index = 0;
-  double bmi = 45;
+  double bmi = 0;
   List<Map<String, dynamic>> workPlan = workoutPlans;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getBMIofUser(FirebaseAuth.instance.currentUser!.uid);
+  }
+
+  getBMIofUser(String id) async {
+    DocumentSnapshot userSnapshot =
+        await FirebaseFirestore.instance.collection('users').doc(id).get();
+    Map<String, dynamic> userData = userSnapshot.data() as Map<String, dynamic>;
+    setState(() {
+      bmi = userData['BMI'];
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,86 +50,3 @@ class _GuidanceScreenState extends State<GuidanceScreen> {
   }
 }
 
-// class GuidanceDetail extends StatelessWidget {
-//   final List<Map<String, dynamic>> guidanceData;
-// 
-//   GuidanceDetail(this.guidanceData);
-// 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Poor BMI Workout Plan'),
-//       ),
-//       body: ListView.builder(
-//         itemCount: guidanceData.length,
-//         itemBuilder: (context, index) {
-//           final plan = guidanceData[index];
-//           final name = plan['name'];
-//           final days = plan['days'];
-// 
-//           return Padding(
-//             padding: const EdgeInsets.all(10.0),
-//             child: Card(
-//               elevation: 5.0,
-//               shape: RoundedRectangleBorder(
-//                 borderRadius: BorderRadius.circular(15.0),
-//               ),
-//               child: Column(
-//                 children: [
-//                   Padding(
-//                     padding: const EdgeInsets.all(10),
-//                     child: Text(
-//                       name,
-//                       style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-//                     ),
-//                   ),
-//                   Column(
-//                     children: days.entries.map((day) {
-//                       final dayName = day.key;
-//                       final dayData = day.value;
-//                       final warmup = dayData['Warm-up'];
-//                       final workout = dayData['Workout'];
-//                       final coolDown = dayData['Cool-down'];
-// 
-//                       return Card(
-//                         margin: EdgeInsets.all(10),
-//                         child: Padding(
-//                           padding: const EdgeInsets.all(10),
-//                           child: Column(
-//                             crossAxisAlignment: CrossAxisAlignment.start,
-//                             children: [
-//                               Text(
-//                                 dayName,
-//                                 style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-//                               ),
-//                               SizedBox(height: 8),
-//                               Text("Warm-up: $warmup"),
-//                               if (workout is List)
-//                                 Column(
-//                                   children: workout.map((exercise) {
-//                                     return ListTile(
-//                                       leading: Icon(Icons.arrow_right, size: 30),
-//                                       title: Text(exercise),
-//                                       minLeadingWidth: 0,
-//                                     );
-//                                   }).toList(),
-//                                 ),
-//                               if (workout is String) Text("Workout: $workout"),
-//                               SizedBox(height: 8),
-//                               Text("Cool-down: $coolDown"),
-//                             ],
-//                           ),
-//                         ),
-//                       );
-//                     }).toList(),
-//                   ),
-//                 ],
-//               ),
-//             ),
-//           );
-//         },
-//       ),
-//     );
-//   }
-// }
