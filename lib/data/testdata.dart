4e7aea26a1a35061299f8917e8615ed4188c2897
Heet Kheni <heetkheni47@gmail.com>
2023-11-04 10:17:25 +0530

    testdata added

---
+++ lib/data/testdata.dart
@@ -0,0 +1,195 @@
+
+  List<Map<String, String>> allTests = [
+    {
+      'name': 'Complete Blood Count (CBC)',
+      'description': 'Measures the number of red blood cells, white blood cells, and platelets in the blood. Provides information about overall health and can help diagnose various conditions.'
+    },
+    {
+      'name': 'Blood Glucose Test',
+      'description': 'Evaluates the concentration of glucose (sugar) in the blood. Used to diagnose and manage diabetes.'
+    },
+    {
+      'name': 'Urinalysis',
+      'description': 'Examines the physical and chemical properties of urine, including color, clarity, pH, and the presence of substances such as glucose and protein. Helps assess kidney function and detect urinary tract infections.'
+    },
+    {
+      'name': 'Urine Culture and Sensitivity (C&S)',
+      'description': 'Cultures a urine sample to identify and determine the antibiotic sensitivity of bacteria causing urinary tract infections.'
+    },
+
+    {
+      'name': 'Sputum Culture',
+      'description': 'Cultures a sample of sputum (mucus from the respiratory tract) to identify and diagnose respiratory infections, such as bacterial pneumonia.'
+    },
+    {
+      'name': 'Acid-Fast Bacillus (AFB) Test',
+      'description': 'Detects the presence of acid-fast bacilli in sputum, indicating infections caused by mycobacteria, such as tuberculosis.'
+    },
+
+    {
+      'name': 'X-rays',
+      'description': 'Uses electromagnetic radiation to create images of the inside of the body. Commonly used for bone examinations and chest imaging.'
+    },
+    {
+      'name': 'Computed Tomography (CT) Scan',
+      'description': 'Produces detailed cross-sectional images of the body using X-rays and a computer. Provides detailed views of organs, bones, and tissues.'
+    },
+    {
+      'name': 'Magnetic Resonance Imaging (MRI)',
+      'description': 'Uses powerful magnets and radio waves to generate detailed images of the body\'s internal structures, providing clear views of soft tissues.'
+    },
+    {
+      'name': 'Ultrasound',
+      'description': 'Uses sound waves to create images of organs and structures inside the body. Commonly used for prenatal imaging, cardiac evaluations, and abdominal examinations.'
+    },
+    {
+      'name': 'Mammogram',
+      'description': 'X-ray examination of the breast to detect and diagnose breast cancer. Mammography is an essential tool for breast cancer screening.'
+    },
+    {
+      'name': 'Bone Density Test (DEXA Scan)',
+      'description': 'Measures bone mineral density to assess the risk of fractures and osteoporosis. Commonly used to evaluate bone health in postmenopausal women.'
+    },
+
+    {
+      'name': 'Electrocardiogram (ECG or EKG)',
+      'description': 'Records the electrical activity of the heart over a period of time. Helps diagnose heart rhythm abnormalities and assess heart health.'
+    },
+    {
+      'name': 'Stress Test (Exercise Treadmill Test)',
+      'description': 'Evaluates the heart\'s response to stress or exercise. Monitors heart rate, blood pressure, and electrocardiogram changes during physical activity.'
+    },
+    {
+      'name': 'Echocardiogram',
+      'description': 'Uses sound waves to create detailed images of the heart. Provides information about the heart\'s structure, function, and blood flow.'
+    },
+    {
+      'name': 'Holter Monitor',
+      'description': 'A portable device that continuously monitors and records the heart\'s electrical activity over 24 to 48 hours. Used for detecting irregular heart rhythms.'
+    },
+    {
+      'name': 'Upper Gastrointestinal (GI) Series',
+      'description': 'Involves X-ray imaging of the upper digestive tract after swallowing a contrast medium. Helps diagnose conditions affecting the esophagus, stomach, and small intestine.'
+    },
+    {
+      'name': 'Colonoscopy',
+      'description': 'Examines the inside of the colon (large intestine) using a flexible tube with a camera. Used for colorectal cancer screening and to diagnose digestive issues.'
+    },
+    {
+      'name': 'Endoscopy',
+      'description': 'Uses a flexible tube with a light and camera to examine the digestive tract, including the esophagus, stomach, and duodenum. Aids'
+    },
+
+    {
+      'name': 'Liver Biopsy',
+      'description': 'Involves the removal of a small piece of liver tissue for examination. Helps diagnose liver diseases and assess liver function.'
+    },
+
+    {
+      'name': 'Pulmonary Function Test (PFT)',
+      'description': 'Measures the function of the lungs, including airflow, lung volume, and how well the lungs exchange gases.'
+    },
+    {
+      'name': 'Bronchoscopy',
+      'description': 'Examines the airways (bronchi) using a flexible tube with a light and camera. Used to diagnose lung conditions.'
+    },
+    {
+      'name': 'Arterial Blood Gas (ABG) Test',
+      'description': 'Measures the levels of oxygen and carbon dioxide in the blood, providing information about lung function and acid-base balance.'
+    },
+
+    {
+      'name': 'DNA Testing',
+      'description': 'Analyzes an individual\'s DNA to identify genetic variations and mutations. Used for various purposes, including ancestry testing and genetic disease risk assessment.'
+    },
+    {
+      'name': 'Carrier Testing',
+      'description': 'Determines if an individual carries a gene mutation associated with a genetic disorder. Often performed before or during pregnancy.'
+    },
+    {
+      'name': 'Genetic Counseling',
+      'description': 'Provides information and support to individuals and families regarding genetic conditions, testing, and potential risks.'
+    },
+
+    {
+      'name': 'Human Immunodeficiency Virus (HIV) Test',
+      'description': 'Detects antibodies or antigens related to HIV infection. Used for screening and diagnosis.'
+    },
+    {
+      'name': 'Tuberculosis (TB) Test',
+      'description': 'Tests for the presence of Mycobacterium tuberculosis, the bacteria that causes tuberculosis.'
+    },
+    {
+      'name': 'Hepatitis Panel',
+      'description': 'Tests for various hepatitis viruses (such as hepatitis A, B, and C) to assess liver health.'
+    },
+  
+    {
+      'name': 'Follicle-Stimulating Hormone (FSH)',
+      'description': 'Measures FSH levels, which play a role in the regulation of the reproductive system.'
+    },
+    {
+      'name': 'Thyroid-Stimulating Hormone (TSH)',
+      'description': 'Measures TSH levels to assess thyroid function.'
+    },
+    {
+      'name': 'Testosterone Levels',
+      'description': 'Measures testosterone levels, important for male reproductive health and other functions.'
+    },
+    {
+      'name': 'Progesterone Levels',
+      'description': 'Measures progesterone levels, important for the menstrual cycle and pregnancy.'
+    },
+    {
+      'name': 'Estradiol Levels',
+      'description': 'Measures estradiol levels, a form of estrogen, important for female reproductive health.'
+    },
+
+    {
+      'name': 'Skin Prick Test',
+      'description': 'Introduces small amounts of allergens into the skin to identify specific allergies.'
+    },
+    {
+      'name': 'Blood Allergy Test (IgE Test)',
+      'description': 'Measures the levels of immunoglobulin E (IgE) antibodies to identify allergies.'
+    },
+
+    {
+      'name': 'Pap Smear (Cervical Cancer Screening)',
+      'description': 'Collects cells from the cervix to detect abnormal changes that may lead to cervical cancer.'
+    },
+    {
+      'name': 'Prostate-Specific Antigen (PSA) Test',
+      'description': 'Measures PSA levels in the blood to screen for prostate cancer.'
+    },
+  
+    {
+      'name': 'Electroencephalogram (EEG)',
+      'description': 'Records the electrical activity of the brain. Used to diagnose epilepsy and other neurological disorders.'
+    },
+    {
+      'name': 'Lumbar Puncture (Spinal Tap)',
+      'description': 'Involves collecting cerebrospinal fluid from the spinal canal. Used for diagnosing conditions affecting the central nervous system.'
+    },
+
+    {
+      'name': 'Metabolic Panel',
+      'description': 'Measures levels of various substances in the blood, providing information about kidney function, electrolyte balance, and more.'
+    },
+    {
+      'name': 'Glucose Tolerance Test (GTT)',
+      'description': 'Assesses the body\'s ability to regulate glucose (sugar) levels over time. Used for diagnosing diabetes.'
+    },
+
+    {
+      'name': 'Antinuclear Antibody (ANA) Test',
+      'description': 'Detects antibodies that may target the body\'s own cells. Used to diagnose autoimmune diseases.'
+    },
+    {
+      'name': 'Rheumatoid Factor (RF) Test',
+      'description': 'Measures the presence of rheumatoid factor antibodies. Used in the diagnosis of rheumatoid arthritis and other autoimmune conditions.'
+    },
+
+  ];
+
+  
